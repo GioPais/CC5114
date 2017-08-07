d7aa8503f9bc7b6d1686a83b4a4e22aacdd354a2
Giopa <Giopa@DESKTOP-S7VEV72>
2017-08-07 10:47:20 -0400

    logical behaviors implemented

--- CC5114/src/perceptron/Perceptron.java
+++ CC5114/src/perceptron/Perceptron.java
@@ -4,32 +4,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Perceptron {
-	private boolean output;
-	private float bias;
-	ArrayList<Float> inputs;
-	ArrayList<Float> gains;
+	private double bias;
+	private double w1;
+	private double w2;
 	public Perceptron(){
-		this.inputs = new ArrayList<Float>();
-		this.gains = new ArrayList<Float>();
 		this.bias=0;
 	}
+	
 	
 	public Perceptron(float b){
 		this.bias=b;
 	}
 	
-	public boolean getOutput(){
-		if (this.sum() + this.bias <= 0){
+	public double getW1(){
+		return this.w1;
+	}
+	
+	public double getW2(){
+		return this.w2;
+	}
+	
+	public void setW1(double w1){
+		this.w1=w1;
+	}
+	
+	public void setW2(double w1){
+		this.w1=w2;
+	}
+	
+	
+	public boolean getOutput(double x1,double x2){
+		if (this.w1*x1+this.w2*x2 + this.bias <= 0){
 			return false;
 		}
 		else{
 			return true;
 		}
 	}
-
-	private float sum() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public boolean NAND(double x1, double x2){
+		this.w1=-2;
+		this.w1=-2;
+		this.bias=3;
+		return this.getOutput(x1, x2);
+	}
+	
+	public boolean AND(double x1, double x2){
+		this.w1=1;
+		this.w1=1;
+		this.bias=-1.5;
+		return this.getOutput(x1, x2);
+	}
+	
+	public boolean OR(double x1, double x2){
+		this.w1=1;
+		this.w1=1;
+		this.bias=-0.5;
+		return this.getOutput(x1, x2);
 	}
 
+	
 }
