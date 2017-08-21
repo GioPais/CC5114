384fdbdc5d7151d7cc719635fd284790032bcadf
GioPais <gio.pais@hotmail.com>
2017-08-21 02:37:11 -0300

    Clase Perceptron completa. Se agregaron test en el main

--- Python/minitarea1/Perceptron.py
+++ Python/minitarea1/Perceptron.py
@@ -19,12 +19,77 @@ class Perceptron(object):
     	else:
     		return 1
 
+    def NAND(self,x1,x2):
+    	self._w1=-2
+    	self._w2=-2
+    	self._bias=3
+    	return self.get_output(x1,x2)
+
+    def AND(self,x1,x2):
+    	self._w1=1
+    	self._w2=1
+    	self._bias=-1.5
+    	return self.get_output(x1,x2)
+
+    def OR(self,x1,x2):
+    	self._w1=1
+    	self._w2=1
+    	self._bias=-0.5
+    	return self.get_output(x1,x2)
+
+    def XOR(self,x1,x2):
+    	aux=self.NAND(x1,x2)
+    	out1=self.NAND(x1,aux)
+    	out2=self.NAND(aux,x2)
+    	return self.NAND(out1,out2)
+
+    def bit_adder(self,x1,x2):
+    	out=self.XOR(x1,x2)
+    	carryOut=self.AND(x1,x2)
+    	return [carryOut,out]
+
 
 
 
 if __name__ == "__main__":
-    print 'hello world'
-    #sm.execute()
+    
+    p=Perceptron()
+
+    #TEST para comportamientos logicos
+
+    #NAND
+    assert p.NAND(0, 0) == 1
+    assert p.NAND(0, 1) == 1
+    assert p.NAND(1, 0) == 1
+    assert p.NAND(1, 1) == 0
+
+    #AND
+    assert p.AND(0, 0) == 0
+    assert p.AND(0, 1) == 0
+    assert p.AND(1, 0) == 0
+    assert p.AND(1, 1) == 1
+
+    #OR
+    assert p.OR(0, 0) == 0
+    assert p.OR(0, 1) == 1
+    assert p.OR(1, 0) == 1
+    assert p.OR(1, 1) == 1
+
+    #XOR
+    assert p.XOR(0, 0) == 0
+    assert p.XOR(0, 1) == 1
+    assert p.XOR(1, 0) == 1
+    assert p.XOR(1, 1) == 0
+
+    #TEST para sumador de un bit
+
+    assert p.bit_adder(0, 0) == [0,0]
+    assert p.bit_adder(0, 1) == [0,1]
+    assert p.bit_adder(1, 0) == [0,1]
+    assert p.bit_adder(1, 1) == [1,0]
+
+
+
 
 
 
